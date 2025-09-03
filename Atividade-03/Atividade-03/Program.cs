412e4ac5e4815b0da8fd182e30b22d02c11c2859
Vinicius Sampaio <viniciussampsoares@gmail.com>
2025-09-03 10:56:39 -0300

    Feat: O usuário irá digitar a altura e comprimento de um retângulo e em seguida o sistema vai escrever a área desse retângulo

---
+++ Atividade-03/Atividade-03/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Digite a altura do retângulo:");
+float altura = float.Parse(Console.ReadLine());
+Console.WriteLine("Digite o comprimento do retângulo:");
+float comprimento = float.Parse(Console.ReadLine());
+Console.WriteLine($"Então a área desse retângulo é {altura * comprimento}");
