f8292a4267f9b6a4941889c2f12b0bef721c7084
Vinicius Sampaio <viniciussampsoares@gmail.com>
2025-09-03 11:25:37 -0300

    Feat: Votos de um município

---
+++ Atividade-05/Atividade-05/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Quantos eleitores esse Município tem?");
+int eleitores = int.Parse(Console.ReadLine());
+Console.WriteLine("Quantos votos foram válidos?");
+float votosvalidos = float.Parse(Console.ReadLine());
+Console.WriteLine("Quantos votos foram brancos?");
+float votobranco = float.Parse(Console.ReadLine());
+Console.WriteLine("Quantos votos foran nulos?");
+float votonulo = float.Parse(Console.ReadLine());
+Console.WriteLine($"Então nesse município tem {eleitores} eleitores e {(votosvalidos/eleitores)*100}% de votos válidos, {(votobranco/eleitores)*100}% de votos brancos e {(votonulo/eleitores)*100}% de votos nulos.");
