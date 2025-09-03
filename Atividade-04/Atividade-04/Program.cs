1a9f541cdc0b0a8da0d989cc4335859e6b973891
Vinicius Sampaio <viniciussampsoares@gmail.com>
2025-09-03 11:30:23 -0300

    Feat: Idade convertida para dias

--- Atividade-04/Atividade-04/Program.cs
+++ Atividade-04/Atividade-04/Program.cs
@@ -1,3 +1,7 @@
 ﻿Console.WriteLine("Quantos anos você tem?");
 int idade = int.Parse(Console.ReadLine());
-Console.WriteLine($"Então a sua idade em dias é {idade*365}");
+Console.WriteLine("Quantos meses se passaram do seu aniversário?");
+int mes = int.Parse(Console.ReadLine());
+Console.WriteLine("Quantos dias se passaram do seu aniversário?");
+int dia = int.Parse(Console.ReadLine());
+Console.WriteLine($"Então a sua idade em dias é {(idade*365)+(mes*30)+dia}");
