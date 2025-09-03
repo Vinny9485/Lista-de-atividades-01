8e663d9d19b2af6608db690338b81f6260343fb4
Vinicius Sampaio <viniciussampsoares@gmail.com>
2025-09-03 11:44:29 -0300

    Feat: Reajuste salarial

---
+++ Atividade-06/Atividade-06/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Qual é o seu salário?");
+float salario = float.Parse(Console.ReadLine());
+Console.WriteLine("Quanto será o reajuste salarial?");
+float reajuste = float.Parse(Console.ReadLine());
+Console.WriteLine($"Então o reajuste será de {100+reajuste}% e o novo salário será de R${((reajuste/100)*salario)+salario}");
