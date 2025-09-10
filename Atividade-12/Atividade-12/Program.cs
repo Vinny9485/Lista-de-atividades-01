5f256db11a03a4ea14ccce3c25cb4a79b8e3d8ca
Vinny9485 <viniciussampsoares@gmail.com>
2025-09-10 08:54:55 -0300

    Feat: Ver qual número digitado é maior

---
+++ Atividade-12/Atividade-12/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Nesse programa vacê irá digitar dois números DIFERENTES");
+Console.WriteLine("Então digite o primeiro número:");
+double num1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Agora digite o segundo número:");
+double num2 = double.Parse(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine($"O número {num1} é maior que o número {num2}");
+}
+else
+{
+    Console.WriteLine($"O número {num2} é maior que o número {num1}");
+}
