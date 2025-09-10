f5f4a2dfe532541ec77b2f99c72a5c4d93ff0a3d
Vinny9485 <viniciussampsoares@gmail.com>
2025-09-10 08:47:00 -0300

    Feat: Fericar se o voto será obrogatório, ou não, esse ano

---
+++ Atividade-11/Atividade-11/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Em qual ano estamos?");
+int anoatual = int.Parse(Console.ReadLine());
+Console.WriteLine("E em qual ano Você nasceu?");
+int anonascimento =  int.Parse(Console.ReadLine());
+int idade = anoatual -  anonascimento;
+if (idade >= 18)
+{
+    Console.WriteLine("Seu voto é obrigatório a partir desse ano!");
+}
+else
+{
+    Console.WriteLine("Seu voto não é obrogatório esse ano!");
+}
