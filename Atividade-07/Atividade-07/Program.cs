2bbb3c30492889219835a1843613e4b8180ec969
Vinny9485 <viniciussampsoares@gmail.com>
2025-09-10 08:15:05 -0300

    Feat: Verificar se o valor é maior ou menor que 10

---
+++ Atividade-07/Atividade-07/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Digite um número:");
+float num1 = float.Parse(Console.ReadLine());
+if (num1 < 10)
+{
+    Console.WriteLine("É menor que 10!");
+}
+else
+{
+    Console.WriteLine("É maior que 10!");
+}
