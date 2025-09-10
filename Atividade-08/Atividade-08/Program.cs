13f17bee4cd00ea8616bffdcf47782cd0fdfa7d6
Vinny9485 <viniciussampsoares@gmail.com>
2025-09-10 08:23:30 -0300

    Feat: Verificar se o número é positivo ou negativo

---
+++ Atividade-08/Atividade-08/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Digite um valor que não seja 0");
+double num1 = double.Parse(Console.ReadLine());
+if (num1 < 0)
+{
+    Console.WriteLine("Esse número é negativo!");
+}
+else
+{
+    Console.WriteLine("Esse número é positivo!");
+}
