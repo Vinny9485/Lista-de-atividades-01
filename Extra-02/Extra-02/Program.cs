aebe32c8c29e2eaa47ad7f21e7b9531624660ace
Vinny9485 <viniciussampsoares@gmail.com>
2025-09-10 10:41:20 -0300

    Feat: Calcular quantos litros de tinta por m²

---
+++ Extra-02/Extra-02/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("FAVOR DIGITAR TODOS OS VALORES EM METROS");
+Console.WriteLine("----------------------------------------");
+Console.WriteLine("Qual é a altura da parede?");
+double altura = double.Parse(Console.ReadLine());
+Console.WriteLine("Qual é o comprimento da parede?");
+double comprimento = double.Parse(Console.ReadLine());
+double area = altura * comprimento;
+Console.WriteLine($"Então a área da parede é de {area}m² e irá precisar de {area * 2} litros de tinta.");
