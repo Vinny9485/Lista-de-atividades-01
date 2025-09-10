880a183bdaf050390c519c95d2e842c5a049cc86
Vinny9485 <viniciussampsoares@gmail.com>
2025-09-10 08:37:36 -0300

    Feat: Desconto em dúzias de maçãs

---
+++ Atividade-09/Atividade-09/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Quantas maçãs você quer comprar?");
+int quantidade = int.Parse(Console.ReadLine());
+if (quantidade < 12)
+{
+    Console.WriteLine($"Então o valor da sua compra será de R${quantidade * 1.30}");
+}
+else
+{
+    Console.WriteLine($"Então o valor da sua compra será de R${quantidade * 1.00}");
+}
