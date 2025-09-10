4c4a639d82ba025a358770b10ce83ac97c6398d1
Vinny9485 <viniciussampsoares@gmail.com>
2025-09-10 08:31:47 -0300

    Feat: Sistema de aprovação escolar

---
+++ Atividade-10/Atividade-10/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Qual foi a sua nota da 1ª avaliação?");
+double nota1 = double.Parse(Console.ReadLine());
+Console.WriteLine("E qual foi a sua nota da 2ª avaliação?");
+double nota2 =  double.Parse(Console.ReadLine());
+double media = (nota1 + nota2) / 2;
+if (media >= 6)
+{
+    Console.WriteLine("Você está aprovado!");
+}
+else
+{
+    Console.WriteLine("Você está reprovado! :(");
+}
