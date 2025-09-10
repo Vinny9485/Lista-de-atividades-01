bd474e64b6d2252a1eec77d05b28b413f0537603
Vinny9485 <viniciussampsoares@gmail.com>
2025-09-10 11:28:55 -0300

    Feat: Calcula o aumento em cima de um salário

---
+++ Extra-05/Extra-05/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("Qual é o seu salário?");
+double salario = double.Parse(Console.ReadLine());
+Console.WriteLine($"Então o seu novo salário com 15% de aumento é de R${salario * 1.15}");
