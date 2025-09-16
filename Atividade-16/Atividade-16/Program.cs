632b0ae24d792100d97d1385a59bbdfa01f61faa
Vinny9485 <viniciussampsoares@gmail.com>
2025-09-16 09:53:33 -0300

    Feat: Fatorial de um número

---
+++ Atividade-16/Atividade-16/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Digite uma número que não seja negativo");
+int num = int.Parse(Console.ReadLine());
+if (num > 0)
+{
+    int i = 1;
+    int fat = 1;
+    while (i <= num)
+    {
+        fat = fat * i;
+        i++;
+    }
+    Console.WriteLine($"Fatorial de {num} é igual a: {fat}");
+}
+else if (num == 0)
+{
+    Console.WriteLine($"{num}! = 1");
+}
+else
+{
+    Console.WriteLine("Fatorial negativo não existe!");
+}
+
