9402e3596abd8425df28815bfd7c7c9738441772
Vinny9485 <viniciussampsoares@gmail.com>
2025-09-16 08:41:00 -0300

    Feat: Mostrar os números entre 1000 e 2000 além de destacar os números que quando divididos por 11 dão resto 5

---
+++ Atividade-15/Atividade-15/Program.cs
@@ -0,0 +1,11 @@
+﻿int num = 1000;
+int divisor = 11;
+while (num<= 2000)
+{
+    Console.WriteLine($"{num}");
+    if (num % divisor == 5)
+    {
+        Console.WriteLine($"Então {num} dividido por {divisor} produzem resto 5");
+    }
+    num++;
+}
